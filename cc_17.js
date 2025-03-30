e0f6a45524f611742f68926bc8ac2208d60f9e53
linndonn <linndonn@usf.edu>
2025-03-30 16:12:57 -0400

    Sales rep’s clients and total spent for a specific client.

--- cc_17.js
+++ cc_17.js
@@ -11,3 +11,26 @@ class Customer {
     }
     getTotalSpent () { return this.purchaseHistory.reduce((total, amount) => total + amount, 0); 
 }}
+
+//Task 2: Create a SalesRep Class
+// Set Properties: name, clients
+class SalesRep {
+    constructor(name) {
+        this.name = name;
+        this.clients = [];
+    }
+
+// Add a method to add a client to the clients array
+    addClient(customer) {
+        this.clients.push(customer);
+    }
+//Finds client by name and returns total spent
+    clientGetTotal (name) {
+        const client = this.clients.find(client => client.name === name);
+        if (client) {
+            return client.getTotalSpent();
+        } else {
+            return `Client does not exist.`;
+        }
+    }
+}
